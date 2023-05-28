f320b2169fdc2e885659e3984e80f448df225a51
Stevenz <kikexgaz@gmail.com>
2023-05-28 11:42:22 -0300

    up

--- src/assets/components/Contacs.jsx
+++ src/assets/components/Contacs.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { UilLinkedin,UilWhatsapp, UilImport } from '@iconscout/react-unicons';
 
+const scrollToTop = () =>{
+  window.scrollTo({
+    top: 0, 
+    behavior: 'smooth'
+    /* you can also use 'auto' behaviour
+       in place of 'smooth' */
+  });
+};
+
+
 const Contacs = () => {
   return (
   <div>
@@ -57,7 +67,7 @@ const Contacs = () => {
      </section>
       <div className="flex justify-center py-12 ">
           <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
-            <a href="#Hero" >
+            <a  onClick={()=>scrollToTop()} >
             <svg className="w-6 h-6 text-violet-500 rotate-180" fill="none" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
               <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
             </svg>
